5f7e90fe957a4fd6d4701b33ccd744bb6265261f
ItColourPen <1009307750@qq.com>
2016-04-29 01:01:47 +0800

    add the spyder of newrank wechat article

---
+++ the_article_newrank.py
@@ -0,0 +1,32 @@
+
+#coding:utf-8
+import requests
+import json
+from bs4 import BeautifulSoup
+def get_url(i,article):
+      url='http://www.newrank.cn/xdnphb/data/getAccountArticle'
+      s=requests.session()
+      data={'nonce':'7831bad1c',
+            'uuid':'B06CF7ADA77947B013CAF3F9DDDB1E61',
+            'xyz':'2e220fadce70d467b9e427d14464daa4'
+            }
+      headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'}
+      source=s.post(url,data=data,headers=headers)
+      js=json.loads(source.content)
+      value=js['value']
+      if article=='lastestArticle':
+            lastestArticle=value[article]
+            return lastestArticle[i]['url']
+      else:
+            topArticle=value[article]
+            return topArticle[i]['url']
+def spyder(url):
+      headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'}
+      source=requests.get(url,headers=headers)
+      soup=BeautifulSoup(source.text)
+      c=soup.find_all('p')
+      for each in c:
+            print each.get_text()
+for each in ('lastestArticle','topArticle'):
+      for i in range(0,10):
+            spyder(get_url(i,each))
